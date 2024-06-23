381b52ab7f189c1a3d1551920e8b3b35e4e85e60
Jose Villarroel <jose.villarroel@lodgify.com>
2024-06-23 18:44:02 +0200

    feat(app): logout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/ui";
-import {
-  api,
-  DatabaseContext,
-  DatabaseDispatchContext,
-  useDb,
-} from "@/collections";
+import { DatabaseContext, DatabaseDispatchContext, useDb } from "@/collections";
+import { DatabaseActionsTypes } from "@/collections/db/use-db";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,10 +12,46 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const { isAuthed } = api.useAuth();
   const { db, dispatchDb } = useDb();
 
-  const navbarElements = isAuthed ? "something" : <></>;
+  const navbarElements = db.user.username ? (
+    <div className="dropdown dropdown-end">
+      <div
+        tabIndex={0}
+        role="button"
+        className="btn btn-ghost btn-circle avatar"
+      >
+        <div className="rounded-full">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="size-6"
+          >
+            <path
+              fillRule="evenodd"
+              d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </div>
+      </div>
+      <ul
+        tabIndex={0}
+        className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+      >
+        <li
+          onClick={() =>
+            dispatchDb({ type: DatabaseActionsTypes.LOGOUT, payload: {} })
+          }
+        >
+          <a>Logout</a>
+        </li>
+      </ul>
+    </div>
+  ) : (
+    <></>
+  );
 
   return (
     <html lang="en">
